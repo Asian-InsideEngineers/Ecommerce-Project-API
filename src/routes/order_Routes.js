1167ef8ad84042005123b9fc7e692c5909f54848
Asian-InsideEngneers <insideengineers2611@gmail.com>
2023-10-23 14:53:18 +0530

    package.json is also neglecgted

--- src/routes/order_Routes.js
+++
@@ -1,8 +0,0 @@
-const order_Routes = require("express").Router();
-const order_Controllers = require("./../controllers/order_Controller");
-
-order_Routes.post("/", order_Controllers.create_Order);
-order_Routes.get("/:CustomerId", order_Controllers.fetch_orders);
-order_Routes.put("/updateOrderStatus", order_Controllers.update_Orders);
-
-module.exports = order_Routes;
